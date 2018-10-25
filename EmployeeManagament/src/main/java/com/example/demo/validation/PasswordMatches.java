6a2d866082048e0380ca804ce08c58a3c1b0358b
at-thaile <thai.le@asiantech.vn>
2018-10-25 09:56:23 +0700

    push properties file

--- EmployeeManagament/src/main/java/com/example/demo/validation/PasswordMatches.java
+++ EmployeeManagament/src/main/java/com/example/demo/validation/PasswordMatches.java
@@ -1,10 +1,13 @@
 package com.example.demo.validation;
 
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.TYPE;
+
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import static java.lang.annotation.ElementType.*;
+
 import javax.validation.Constraint;
 import javax.validation.Payload;
 
